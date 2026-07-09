5f04497ddf67108f1a2c205fe1b3fadd6f4bc6ae
Shreya Sharma <165395224+Shreyaaaaa07@users.noreply.github.com>
2026-07-10 01:48:26 +0530

    Add: find-peak-element - submission-6

---
+++ Data Structures & Algorithms/find-peak-element/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+
+        // Handle edge cases
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        int st = 1;
+        int end = n - 2;
+        while(st<= end){
+            int mid = st +(end-st)/2;
+
+            if(nums[mid-1]<nums[mid] && nums[mid]> nums[mid+1]){
+                return mid;
+            }
+
+            if (nums[mid-1]<nums[mid]){
+                st = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return st;
+        
+    }
+};
